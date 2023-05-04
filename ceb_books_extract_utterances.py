2da107ba02af57fd235760f7ccd6f685b5b2cdc2
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-04 20:54:17 +0300

    upd

--- ceb_books_extract_utterances.py
+++ ceb_books_extract_utterances.py
@@ -1,8 +1,7 @@
 import os
-
 from utils_ceb import CEBApi
 
 # Using a side project `gutenberg-dialog` for utterances extraction.
-cmd = "python3 service/gutenberg-dialog/code/main.py -l=en -f1 -e -f2 -c -dir {books_dir}".format(
+cmd = "python -m gutenberg_dialog.main -l=en -f1 -e -f2 -c -dir {books_dir}".format(
     books_dir=CEBApi.books_storage)
 os.system(cmd)
--- service/download.sh
+++
@@ -1,2 +0,0 @@
-# This project is related to dialogs extraction from project Gutenberg.
-git clone git@github.com:nicolay-r/gutenberg-dialog.git
