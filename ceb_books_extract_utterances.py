4516602850de77acdb559a9e3ff3789ae4afebf2
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-09 15:14:50 +0300

    Cleanining empty books

--- ceb_books_extract_utterances.py
+++ ceb_books_extract_utterances.py
@@ -1,7 +1,12 @@
 import os
 from utils_ceb import CEBApi
 
+books_dir = CEBApi.books_storage
+
 # Using a side project `gutenberg-dialog` for utterances extraction.
-cmd = "python -m gutenberg_dialog.main -l=en -f1 -e -f2 -c -dir {books_dir}".format(
-    books_dir=CEBApi.books_storage)
+cmd = "python -m gutenberg_dialog.main -l=en -f1 -e -f2 -c -dir {books_dir}".format(books_dir=books_dir)
+os.system(cmd)
+
+# remove empty files.
+cmd = "find {books_dir} -type f -empty -print -delete".format(books_dir=books_dir)
 os.system(cmd)
