bdabe2a659fe3454e1d5a3fee5bd3e5b966a1931
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-09 16:52:28 +0100

    Added my API, and annotation of utterances based on texts with annotated characters

--- ceb_books_extract_utterances.py
+++ ceb_books_extract_utterances.py
@@ -1,7 +1,12 @@
 import os
+from utils_my import MyAPI
 from utils_ceb import CEBApi
 
 # Using a side project `gutenberg-dialog` for utterances extraction.
-cmd = "python -m gutenberg_dialog.main -l=en -f1 -e -f2 -c -dir {books_dir}".format(
-    books_dir=CEBApi.books_storage)
+
+# Origin.
+books_dir = CEBApi.books_storage_en
+cmd = "python3 -m gutenberg_dialog.main -l=en -f1 -e -f2 -dir {books_dir}".format(
+    books_dir=MyAPI.books_storage)
+
 os.system(cmd)
