cb0861656f107a22b5cc79b303736489a1b06360
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-11 18:07:57 +0100

    Provided FCP api and the related analysis

---
+++ ceb_books_personalities.py
@@ -0,0 +1,35 @@
+from tqdm import tqdm
+from os import listdir
+from os.path import isfile, join
+from utils_fcp import FcpApi
+from utils_my import MyAPI
+
+fcp_api = FcpApi()
+
+
+d = fcp_api.extract_as_lexicon()
+for key, value in d.items():
+    print(key, value)
+
+in_dir = MyAPI.books_storage_en
+f_names = [f for f in listdir(in_dir) if isfile(join(in_dir, f))]
+
+lexicon_entries = [next(iter(v["low"])) for v in d.values()] + [next(iter(v["high"])) for v in d.values()]
+print(lexicon_entries)
+
+c = 0
+stat = {}
+for f_name in tqdm(f_names):
+    with open(join(in_dir, f_name), "r") as f:
+        for line in f.readlines():
+            line_words = line.split(' ')
+            for w in line_words:
+                if w in lexicon_entries:
+                    if w not in stat:
+                        stat[w] = 0
+                    stat[w] += 1
+
+for k, v in sorted(stat.items(), key=lambda item: item[1]):
+    print(k, v)
+print("Entries matched: {}".format(sum(stat.values())))
+
---
+++ utils_fcp.py
@@ -0,0 +1,33 @@
+from os.path import join, dirname, realpath
+
+import pandas as pd
+
+
+class FcpApi:
+    """ Fictional character personalities API.
+    """
+
+    __current_dir = dirname(realpath(__file__))
+    personalities = join(__current_dir, "./data/fictional-character-personalities/personalities.txt")
+
+    def extract_as_lexicon(self, path=None):
+        assert(isinstance(path, str) or path is None)
+        path = self.personalities if path is None else path
+        df = pd.read_csv(path, sep="\t")
+
+        lexicon = {}
+        lexicon_df = df[["spectrum", "spectrum_low", "spectrum_high"]]
+        for _, row in lexicon_df.iterrows():
+            r = row.to_dict()
+
+            spectrum = r["spectrum"]
+
+            # seeking for existed.
+            if spectrum not in lexicon:
+                lexicon[spectrum] = {"low": set([r["spectrum_low"]]), "high": set([r["spectrum_high"]])}
+            else:
+                meta = lexicon[spectrum]
+                meta["low"].add(r["spectrum_low"])
+                meta["high"].add(r["spectrum_high"])
+
+        return lexicon
