754d6f3df6900318c9dcd7a1366b5f95feb2882d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-11 12:03:09 +0100

    Update connections extraction

--- ceb_books_parse_char_connections.py
+++ ceb_books_parse_char_connections.py
@@ -33,11 +33,12 @@ from arekit.contrib.utils.synonyms.simple import SimpleSynonymCollection
 from utils_my import MyAPI
 
 
-class FilesDocOperations(DocumentOperations):
-    """ Document Operations based on the list of provided file paths.
+class DirectoryFilesDocOperations(DocumentOperations):
+    """ Document Operations based on the list of provided file paths
+        for the particular directory.
     """
 
-    def __init__(self, file_paths=None, sentence_parser=None):
+    def __init__(self, dir_path, file_names=None, sentence_parser=None):
         """
             file_paths: list
                 list of file paths related to documents.
@@ -45,20 +46,20 @@ class FilesDocOperations(DocumentOperations):
                 how data is suppose to be separated onto sentences.
                 str -> list(str)
         """
-
-        assert(isinstance(file_paths, list) or file_paths is None)
+        assert(isinstance(dir_path, str))
+        assert(isinstance(file_names, list) or file_names is None)
         assert(callable(sentence_parser) or sentence_parser is None)
 
-        self.__docs_paths = file_paths
+        self.__dir_path = dir_path
+        self.__file_names = file_names
 
         # Line-split sentence parser by default.
         self.__sentence_parser = lambda text: [t.strip() for t in text.split('\n')] \
             if sentence_parser is None else sentence_parser
 
     def __read_doc(self, doc_id, contents):
-        """ input_data: list
+        """ Parse a single document.
         """
-
         # setup input data.
         sentences = self.__sentence_parser(contents)
         sentences = list(map(lambda text: BaseNewsSentence(text), sentences))
@@ -69,9 +70,13 @@ class FilesDocOperations(DocumentOperations):
     def get_doc(self, doc_id):
         """ Perform reading operation of the document.
         """
-        with open(self.__docs_paths[doc_id], "r") as f:
+        file_name = self.__file_names[doc_id]
+        with open(join(self.__dir_path, file_name), "r") as f:
             contents = f.read()
-            return self.__read_doc(doc_id=doc_id, contents=contents)
+            return self.__read_doc(doc_id=file_name, contents=contents)
+
+    def __len__(self):
+        return len(self.__file_names)
 
 
 class CEBTextEntitiesParser(BasePipelineItem):
@@ -128,9 +133,10 @@ pipeline = BasePipeline([
 #####
 # Declaring pipeline related context parameters.
 #####
-no_folding = NoFolding(doc_ids=[0, 1], supported_data_type=DataType.Train)
+doc_ops = DirectoryFilesDocOperations(
+    dir_path=in_dir, file_names=[f for f in listdir(in_dir) if isfile(join(in_dir, f))])
 
-doc_ops = FilesDocOperations(file_paths=[join(in_dir, f) for f in listdir(in_dir) if isfile(join(in_dir, f))])
+no_folding = NoFolding(doc_ids=range(len(doc_ops)), supported_data_type=DataType.Train)
 
 text_parser = BaseTextParser(pipeline=[
     TermsSplitterParser(),
