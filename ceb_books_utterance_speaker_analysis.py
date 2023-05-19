984a480ae59a0afa2fb72202715a83a27312ad22
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-19 14:52:04 +0100

    expanded analysis

--- ceb_books_utterance_speaker_analysis.py
+++ ceb_books_utterance_speaker_analysis.py
@@ -9,15 +9,52 @@ gd_api = GuttenbergDialogApi()
 books_count = my_api.books_count()
 print(books_count)
 
-# Collect.
-for k in [None] + list(range(20)):
-    book_dialogs = {}
-    for book_id, lines in gd_api.filter_utt_with_speaker_at_k(book_path_func=my_api.get_book_path, k=k):
-        if book_id not in book_dialogs:
-            book_dialogs[book_id] = []
-        book_dialogs[book_id].append(lines)
 
-    books_total = len(book_dialogs)
-    avg = sum([len(book_dialogs) for book_dialogs in book_dialogs.values()]) / books_count
-    print("At least one segment in dialog mention author at position {k} (avg/book): ".format(k=k), round(avg, 2),
-          "[{} of {}]".format(books_total, books_count))
+def calc_comments_count(c):
+    count = 0
+    for v in c.values():
+        count += sum([len(a) for a in v if isinstance(a, list)])
+    return count
+
+
+def total_dialogs_count(c):
+    return sum([len(v) for v in c.values()])
+
+
+# Assess total amount of extracted comments.
+all_dialogs = {}
+for book_id, lines in gd_api.filter_comment_with_speaker_at_k(book_path_func=my_api.get_book_path, k=None):
+    if book_id not in all_dialogs:
+        all_dialogs[book_id] = []
+    all_dialogs[book_id].append(lines)
+total_comments_count = calc_comments_count(all_dialogs)
+
+print("------------------")
+
+# Collect.
+for k in list(range(10)):
+    k_comments = {}
+    for book_id, lines in gd_api.filter_comment_with_speaker_at_k(book_path_func=my_api.get_book_path, k=k):
+        if book_id not in k_comments:
+            k_comments[book_id] = []
+        k_comments[book_id].append(lines)
+
+    print("COMMENTS with mentioned author at position {k} (%): ".format(k=k),
+          round(100.0 * calc_comments_count(k_comments) / calc_comments_count(all_dialogs), 2))
+
+print("------------------")
+
+# Collect.
+for k in [None] + list(range(10)):
+
+    k_dialogs = {}
+    for book_id, lines in gd_api.filter_comment_with_speaker_at_k(book_path_func=my_api.get_book_path, k=k):
+        if book_id not in k_dialogs:
+            k_dialogs[book_id] = []
+        k_dialogs[book_id].append(lines)
+
+    books_total = len(k_dialogs)
+
+    print("At least one segment in DIALOG mention author at position {k} (% of dialogs): ".format(k=k),
+          round(100.0 * total_dialogs_count(k_dialogs) / total_dialogs_count(all_dialogs), 2),
+          "[{}% of books]".format(100.0 * len(k_dialogs)/ len(all_dialogs)))
