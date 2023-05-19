a420e624da37e9791e197a8a666838bd958171f9
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-19 16:38:24 +0100

    added annotation of speakers!

--- utils_gd.py
+++ utils_gd.py
@@ -7,6 +7,7 @@ from core.book_dialog import BookDialogueService
 
 class GuttenbergDialogApi:
 
+    __tokenizer = RegexpTokenizer(r'\w+')
     __current_dir = dirname(realpath(__file__))
     dialogs_en = join(__current_dir, "./data/filtered/en/dialogs_clean.txt")
 
@@ -37,10 +38,21 @@ class GuttenbergDialogApi:
                     bs.set_paragraphs(l_from=l_from, l_to=l_to)
                     bs.register_utterance(utt=utt, l_from=l_from, l_to=l_to)
 
-    def filter_utt_with_speaker_at_k(self, book_path_func, k=None):
+    def normalize_terms(self, terms):
+        """ apply cleaning from punctuation signs.
+        """
+        assert(isinstance(terms, list))
+        return self.__tokenizer.tokenize(' '.join(terms))
+
+    @staticmethod
+    def is_character(term):
+        """ followed by CEB api.
+        """
+        return term.count('_') == 2
+
+    def filter_comment_with_speaker_at_k(self, book_path_func, k=None):
         """ filter examples in distance from K
         """
-        tokenizer = RegexpTokenizer(r'\w+')
         for book_id, lines in self.iter_dialog_segments(book_path_func):
             filtered = []
             for segment in lines:
@@ -53,20 +65,19 @@ class GuttenbergDialogApi:
                 terms = segment.split()[1:]
 
                 # Get rid o punctuation.
-                tokenized_terms = tokenizer.tokenize(' '.join(terms))
+                tokenized_terms = self.normalize_terms(terms)
 
                 ######################################################
                 # Check speaker at position K and no speakers behind.
                 ######################################################
                 is_ok = True
-                is_char = lambda term: term.count('_') == 2
                 if k is not None:
                     if k < len(tokenized_terms):
                         for i in range(k):
-                            if is_char(tokenized_terms[i]):
+                            if self.is_character(tokenized_terms[i]):
                                 is_ok = False
                                 break
-                        is_ok = is_ok and is_char(tokenized_terms[k])
+                        is_ok = is_ok and self.is_character(tokenized_terms[k])
                     else:
                         is_ok = False
                 else:
