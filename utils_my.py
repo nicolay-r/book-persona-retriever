d07bac0f33e76e994e626991b608522411e53f1d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-19 16:41:11 +0100

    Fixed prefixes

--- utils_my.py
+++ utils_my.py
@@ -8,6 +8,7 @@ class MyAPI:
 
     __current_dir = dirname(realpath(__file__))
     books_storage = join(__current_dir, "./data/ceb_books_annot")
+    prefixes_storage = join(__current_dir, "./data/ceb_books_annot/prefixes")
     books_storage_en = join(books_storage, "en")
 
     def __init__(self, books_root=None):
