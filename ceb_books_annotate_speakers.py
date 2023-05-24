c8462ba0f1ff5201ea19c475c3a302297943b847
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-24 13:21:06 +0100

    Refactoring code of speaker annotated dialog writing

--- ceb_books_annotate_speakers.py
+++ ceb_books_annotate_speakers.py
@@ -1,63 +1,85 @@
+from collections import OrderedDict
+
 from utils_gd import GuttenbergDialogApi
 from utils_my import MyAPI
 
-gd_api = GuttenbergDialogApi()
+
+def iter_speaker_annotated_dialogs(book_path_func, prefix_lexicon=None):
+    assert(isinstance(prefix_lexicon, set) or prefix_lexicon is None)
+
+    gd_api = GuttenbergDialogApi()
+
+    found = 0
+    for b_id, dialog_segments in gd_api.iter_dialog_segments(book_path_func):
+
+        speakers = set()
+        recognized_speakers = {}
+
+        for i, segment in enumerate(dialog_segments):
+
+            # Considering only comments.
+            if segment[0] not in ['#', '.']:
+                continue
+
+            # Taking meta information and text.
+            terms = segment.split()
+            meta = terms[0]
+            text = terms[1:]
+
+            speaker_id = int(meta[1:-1])
+            speakers.add(speaker_id)
+
+            # Do analysis.
+            terms = gd_api.normalize_terms(text)
+
+            ########################################################
+            # Case 1.
+            # For the case when first term is a mentioned character.
+            ########################################################
+            if len(terms) > 0 and gd_api.is_character(terms[0]):
+                # Provide info.
+                recognized_speakers[speaker_id] = terms[0]
+                found += 1
+
+            ########################################################
+            # Case 2.
+            # Separation with the one word mentioned in between.
+            ########################################################
+            if prefix_lexicon is not None:
+                if len(terms) > 1 and gd_api.is_character(terms[1]) and terms[0] in prefix_lexicon:
+                    # Provide info.
+                    recognized_speakers[speaker_id] = terms[1]
+                    found += 1
+
+            ########################################################
+            # Case 3.
+            # TODO.
+            ########################################################
+
+        # Compose to format of actual dialog between speakers
+        dialog = OrderedDict()
+        for segment in dialog_segments:
+
+            if segment[0] not in [">"]:
+                continue
+
+            # Taking meta information and text.
+            terms = segment.split()
+            meta = terms[0]
+            text = ' '.join(terms[1:])
+
+            # Initialize the list for the particular speaker.
+            speaker_id = int(meta[1:-1])
+            if speaker_id not in dialog:
+                dialog[speaker_id] = []
+
+            dialog[speaker_id].append(text)
+
+        yield dialog, recognized_speakers
+
+
 my_api = MyAPI()
-
-found = 0
-for b_id, dialog in gd_api.iter_dialog_segments(book_path_func=my_api.get_book_path):
-
-    recognized = {}
-
-    for i, segment in enumerate(dialog):
-
-        # Considering only comments.
-        if segment[0] not in ['#', '.']:
-            continue
-
-        # Taking meta information and text.
-        terms = segment.split()
-        meta = terms[0]
-        text = terms[1:]
-
-        speaker_id = int(meta[1:-1])
-
-        # Do analysis.
-        terms = gd_api.normalize_terms(text)
-
-        ########################################################
-        # Case 1.
-        # For the case when first term is a mentioned character.
-        ########################################################
-        if len(terms) > 0 and gd_api.is_character(terms[0]):
-            # Provide info.
-            recognized[speaker_id] = terms[0]
-
-        ########################################################
-        # Case 2.
-        # TODO.
-        ########################################################
-
-        ########################################################
-        # Case 3.
-        # TODO.
-        ########################################################
-
-    # print recognized.
-    for segment in dialog:
-        if segment[0] not in [">"]:
-            continue
-
-        # Taking meta information and text.
-        terms = segment.split()
-        meta = terms[0]
-        text = terms[1:]
-
-        speaker_id = int(meta[1:-1])
-
-        u = "{{{}}}".format(recognized[speaker_id]) + segment \
-            if speaker_id in recognized else segment
-
-        print(u)
-
-print(found)
+# We consider prefix lexicon for recognition of the speakers in texts.
+data_iter = iter_speaker_annotated_dialogs(book_path_func=my_api.get_book_path,
+                                           prefix_lexicon=my_api.load_prefix_lexicon_en())
+my_api.write_annotated_dialogs(iter_dialogs_and_speakers=data_iter)
--- utils_my.py
+++ utils_my.py
@@ -1,5 +1,8 @@
+from collections import OrderedDict
 from os import listdir
 from os.path import join, dirname, realpath, isfile
+
+from tqdm import tqdm
 
 
 class MyAPI:
@@ -9,6 +12,7 @@ class MyAPI:
     __current_dir = dirname(realpath(__file__))
     books_storage = join(__current_dir, "./data/ceb_books_annot")
     prefixes_storage = join(__current_dir, "./data/ceb_books_annot/prefixes")
+    dialogs_filepath = join(__current_dir, "./data/ceb_books_annot/dialogs.txt")  # dialogs storage.
     books_storage_en = join(books_storage, "en")
 
     def __init__(self, books_root=None):
@@ -16,6 +20,19 @@ class MyAPI:
 
     def get_book_path(self, book_id):
         return join(self.__book_storage_root, "{book_id}.txt".format(book_id=book_id))
+
+    @staticmethod
+    def load_prefix_lexicon_en(default_suffix="b500", filepath=None):
+        """ Loading the aux lexicon which allow us recognize characters in text.
+            default suffix is an amount of considered books.
+        """
+        entries = set()
+        filepath = "{}-{}.txt".format(MyAPI.prefixes_storage, default_suffix) if filepath is None else filepath
+        with open(filepath, "r") as f:
+            for line in f.readlines():
+                # split n-gramms.
+                entries.add(line.replace('~', ' '))
+        return entries
 
     def books_count(self):
         count = 0
@@ -25,3 +42,22 @@ class MyAPI:
             if isfile(join(dir_path, path)):
                 count += 1
         return count
+
+    def write_annotated_dialogs(self, iter_dialogs_and_speakers, filepath=None, print_sep=True):
+        filepath = self.dialogs_filepath if filepath is None else filepath
+
+        with open(filepath, "w") as file:
+            for dialog, recognized_speakers in tqdm(iter_dialogs_and_speakers):
+                assert(isinstance(dialog, OrderedDict))
+
+                for speaker_id, segments in dialog.items():
+                    assert(isinstance(segments, list))
+                    assert(isinstance(recognized_speakers, dict))
+
+                    sep = " " if print_sep is False else " [USEP] "
+                    utterance = sep.join(segments)
+                    speaker = recognized_speakers[speaker_id] \
+                        if speaker_id in recognized_speakers else "UNKN-{}".format(speaker_id)
+                    file.write("{speaker}: {utterance}\n".format(speaker=speaker, utterance=utterance))
+
+                file.write('\n')
