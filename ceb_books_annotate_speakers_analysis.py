6ed5a74d23aab946bb5d8985b6fe36733a6640dd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-25 12:38:35 +0100

    refactoring. sep analysis from extraction. added seaborn for detailed analysis.

---
+++ ceb_books_annotate_speakers_analysis.py
@@ -0,0 +1,35 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from core.speaker_annotation import iter_speaker_annotated_dialogs
+from utils_my import MyAPI
+
+my_api = MyAPI()
+
+stat = my_api.calc_annotated_dialogs_stat(
+    iter_dialogs_and_speakers=iter_speaker_annotated_dialogs(
+        book_path_func=my_api.get_book_path,
+        prefix_lexicon=my_api.load_prefix_lexicon_en())
+)
+
+print("recognized/utt: {}%".format(str(round(100.0 * stat["recognized"]/stat["utterances"], 2))))
+print("recognized speakers per dialogs: {}".format(str(round(stat["recognized"]/stat["dialogs"], 2))))
+# print("utterances per speaker: {}".format(str(round(stat["utterances_per_speaker"], 2))))
+print("Total speakers: {}".format(len(stat["speakers_uc_stat"])))
+
+
+df_dict = {
+    "speaker": [],
+    "utts": []
+}
+
+for k, v in stat["speakers_uc_stat"].items():
+    df_dict["speaker"].append(k)
+    df_dict["utts"].append(v)
+speaker_utts_df = pd.DataFrame(df_dict)
+sns.displot(speaker_utts_df, x="utts")
+
+plt.ylim(0, 1000)
+plt.xlim(0, 100)
+plt.show()
--- ceb_books_extract_utterances.py
+++ ceb_books_extract_utterances.py
@@ -5,7 +5,7 @@ from utils_ceb import CEBApi
 # Using a side project `gutenberg-dialog` for utterances extraction.
 
 # Origin.
-books_dir = CEBApi.books_storage_en
+#books_dir = CEBApi.books_storage_en
 cmd = "python3 -m gutenberg_dialog.main -l=en -f1 -e -f2 -dir {books_dir}".format(
     books_dir=MyAPI.books_storage)
 
