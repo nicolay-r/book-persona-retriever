1cdfa6377e1d2c4e435a539b46c239968e3702c3
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-06-08 11:06:34 +0100

    Renaming and reordering to the appropriate sequence of the pipeline steps. Refactoring.

--- my_s2_extract_utterances.py
+++ my_s2_extract_utterances.py
@@ -1,11 +1,9 @@
 import os
 from utils_my import MyAPI
-from utils_ceb import CEBApi
 
 # Using a side project `gutenberg-dialog` for utterances extraction.
 
 # Origin.
-#books_dir = CEBApi.books_storage_en
 cmd = "python3 -m gutenberg_dialog.main -l=en -f1 -e -f2 -dir {books_dir}".format(
     books_dir=MyAPI.books_storage)
 
