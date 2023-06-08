fcb2d2faeffefde14c6540765688e2949a90600c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-06-08 10:55:48 +0100

    Provided lim parameters for plot

--- core/plot.py
+++ core/plot.py
@@ -5,10 +5,13 @@ import seaborn as sns
 from matplotlib import pyplot as plt, ticker
 
 
-def draw_count_plot(c, x_name, cat_name, val_to_x=lambda v: v, val_to_cat=lambda v: 0, interval=10):
+def draw_count_plot(c, x_name, cat_name, val_to_x=lambda v: v, val_to_cat=lambda v: 0, interval=10,
+                    y_lim=None, x_lim=None):
     """ Draw count plot of all BAPS
     """
     assert(isinstance(c, Counter))
+    assert(isinstance(x_lim, tuple))
+    assert(isinstance(y_lim, tuple))
 
     df_dict = {x_name: [], cat_name: []}
     for k, v in c.items():
@@ -22,3 +25,9 @@ def draw_count_plot(c, x_name, cat_name, val_to_x=lambda v: v, val_to_cat=lambda
     ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
 
     plt.show()
+
+    # Additional customizations of the diagram.
+    if x_lim is not None:
+        plt.ylim(x_lim)
+    if y_lim is not None:
+        plt.xlim(y_lim)
