595da69e541e9c0e8f32c8a44bbb9f598090f4c4
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-06-27 16:14:21 +0100

    Refactoring formatter to the facebook deprectated version (following ConvAI)

---
+++ core/utls_parlai_facebook_formatter.py
@@ -0,0 +1,29 @@
+def format_episode(request, response, candidates, resp_persona_traits=None):
+    """ Serializer for the deprecated formatter proposed by Facebook.
+    """
+    assert(isinstance(request, str))
+    assert(isinstance(response, str))
+    assert(isinstance(candidates, list))
+
+    def __handle_line(l):
+        return l.replace('\t', ' ')
+
+    def __fn(a):
+        return filter(lambda item: item is not None, a)
+
+    lines = []
+
+    # Traits
+    if resp_persona_traits is not None:
+        traits = ["your persona: I am {}".format(__handle_line(trait)) for trait in resp_persona_traits]
+        lines.extend(traits)
+
+    # Main episode content. (Query - Response)
+    text = "\n".join(__fn([ __handle_line(request)]))
+    labels = "\n".join([__handle_line(response)])
+    reward = ""
+    label_candidates = "|".join([__handle_line(c) for c in candidates])
+
+    lines.append("\t".join([text, labels, reward, label_candidates]))
+
+    return "\n".join(["{} {}".format(i+1, l) for i, l in enumerate(lines)])
--- my_s5_parlai_dataset_convert.py
+++ my_s5_parlai_dataset_convert.py
@@ -1,31 +1,7 @@
 import zipstream
+
+from core.utls_parlai_facebook_formatter import format_episode
 from utils_my import MyAPI
-
-
-def format_episode(request, response, candidates, resp_persona_traits=None):
-    """ resp_persona_traits:
-            used to pass spectrums and (maybe) other main traits known for sure.
-    """
-    assert(isinstance(request, str))
-    assert(isinstance(response, str))
-    assert(isinstance(candidates, list))
-
-    def __handle_line(l):
-        return l.replace('\t', ' ')
-
-    def __fn(a):
-        return filter(lambda item: item is not None, a)
-
-    req_persona = None
-    if resp_persona_traits is not None:
-        # We follow the Conv2AI format: "Your persona: I am ..."
-        req_persona = "\n".join(["your persona: I am {}".format(__handle_line(trait)) for trait in resp_persona_traits])
-
-    text = "text:" + "\n".join(__fn([req_persona, __handle_line(request)]))
-    labels = "\n".join(["labels:" + __handle_line(response)])
-    label_candidates = "\n".join(["candidates_list:"] + [__handle_line(c) for c in candidates])
-
-    return "\t".join([text, labels, label_candidates, "episode_done:True"]) + "\n"
 
 
 def iter_dataset_lines(my_api):
@@ -46,11 +22,12 @@ def iter_dataset_lines(my_api):
             continue
 
         yield format_episode(request=e[0], response=e[1], candidates=[e[1]]).encode()
+        yield b"\n"
 
 
 my_api = MyAPI()
 z = zipstream.ZipFile()
-z.write_iter('dataset.txt', iter_dataset_lines(my_api))
+z.write_iter('train_original.txt', iter_dataset_lines(my_api))
 with open(my_api.dataset_parlai_filepath, "wb") as f:
     for episode_line in z:
         f.write(episode_line)
