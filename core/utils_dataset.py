54c09dbed09be322083ba1e93d9391b5a7cf1e2b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-04 15:57:24 +0100

    Fixed: added missed file

---
+++ core/utils_dataset.py
@@ -0,0 +1,18 @@
+def filter_query(utterances_it):
+    """ Filer those queries from dataset that initiate the dialogue.
+    """
+    for u in utterances_it:
+        if u is None:
+            continue
+        if not u.startswith("UNKN-"):
+            continue
+        yield u
+
+
+def filter_responses(utterances_it):
+    for u in utterances_it:
+        if u is None:
+            continue
+        if u.startswith("UNKN-"):
+            continue
+        yield u
