5f3d3028de815de5e5af2b1b1beda6e2bd2edc4f
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-05 16:23:31 +0100

    Fixed bug in dataset utterance type filtering rules

--- core/utils_dataset.py
+++ core/utils_dataset.py
@@ -1,21 +1,26 @@
-from utils_my import MyAPI
-
-
 def filter_query(utterances_it):
     """ Filer those queries from dataset that initiate the dialogue.
     """
+
+    buffer = []
     for u in utterances_it:
+        assert(len(buffer) <= 2)
         if u is None:
+            buffer.clear()
             continue
-        if not u.startswith(MyAPI.unknown_speaker):
-            continue
-        yield u
+        buffer.append(u)
+        if len(buffer) == 1:
+            yield u
 
 
 def filter_responses(utterances_it):
+
+    buffer = []
     for u in utterances_it:
+        assert(len(buffer) <= 2)
         if u is None:
+            buffer.clear()
             continue
-        if u.startswith(MyAPI.unknown_speaker):
-            continue
-        yield u
+        buffer.append(u)
+        if len(buffer) == 2:
+            yield u
