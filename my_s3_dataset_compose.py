d6c54e35ea0e6cb966f13b6f28122a8dff6ff483
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-12 12:06:19 +0100

    Fixed bug in stat calc

--- my_s3_dataset_compose.py
+++ my_s3_dataset_compose.py
@@ -27,11 +27,11 @@ print("Speakers considered: {}".format(len(speaker_names_list)))
 cc = Counter({k: c for k, c in stat_origin["speakers_reply_stat"].items()
               if c >= my_api.dataset_min_utterances_per_char})
 
-draw_hist_plot(cc, n_bins=10,
+draw_hist_plot(cc, n_bins=20,
                desc="Speakers reply stat origin",
                save_png_path=join(MyAPI.books_storage, "dataset_speakers_reply_origin.png"),
                show=False, asp_hor=12, asp_ver=2,
-               min_val=0, max_val=len(cc))
+               min_val=0, max_val=my_api.dataset_max_utterances_per_char)
 
 ##########################################################################
 # Filter dialogs to the result dataset. Compose a Question->Response pair.
