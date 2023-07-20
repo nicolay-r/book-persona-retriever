5e207f28f89dc40ed3074ad70e4171c8adcb9b0a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-20 17:10:48 +0100

    Refactoring #17 related

--- test/s4_factor_users_0_melt_data.py
+++ test/s4_factor_users_0_melt_data.py
@@ -8,11 +8,14 @@ from utils_my import MyAPI
 X = list(NpzUtils.load(MyAPI.spectrum_features_norm))
 y = list(NpzUtils.load(MyAPI.spectrum_speakers))
 
-ffunc = lambda val: val > 0.3 or val < -0.3
+filter_func = lambda val: val > 0.3 or val < -0.3
 with open(join(MyAPI.books_storage, "features_melted.txt"), "w") as out:
     out.write("user,feature,value\n")
     for u_ind, name in tqdm(enumerate(y)):
         for item_ind, x in enumerate(X[u_ind]):
-            if not ffunc(x):
-                continue
+            # We use this limitation to guarantee that there is a one criteria for every speaker,
+            # and hence the result amount of speakers will remain the same.
+            if item_ind > 0:
+                if not filter_func(x):
+                    continue
             out.write("{},{},{}\n".format(u_ind, item_ind, round(x, 2)))
