951235b9c1b2a5875bbce8b7575d34d5dc7bfdff
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-08-08 12:14:03 +0100

    Added useful util

---
+++ utils/parse_parlai_results.py
@@ -0,0 +1,21 @@
+# This script allows to parse the following lines:
+
+result_metrics = [
+    "hits@1",
+    "hits@5",
+    "hits@10",
+    "f1",
+    "mrr",
+    "rank",
+    "precision",
+    "recall",
+    "bleu-4",
+]
+
+with open("file.txt", "r") as f:
+    contents = f.read()
+    contents = contents.replace("\n", "").replace("\\", "").strip()
+    tokens = contents.split()
+    r = {tokens[i]: tokens[i+1] for i in range(0, len(tokens), 2)}
+    line = [r[m] if m in r else "" for m in result_metrics]
+    print("\t".join(line))
