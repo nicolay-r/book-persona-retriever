db6ea9695d7fac05d430c53aa0514a927f3e4018
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-08-15 14:38:38 +0100

    Fixed bugs

--- utils/parse_parlai_results.py
+++ utils/parse_parlai_results.py
@@ -14,7 +14,7 @@ result_metrics = [
 
 with open("file.txt", "r") as f:
     contents = f.read()
-    contents = contents.replace("\n", "").replace("\\", "").strip()
+    contents = contents.replace("\n", " ").replace("\\", " ").strip()
     tokens = contents.split()
     r = {tokens[i]: tokens[i+1] for i in range(0, len(tokens), 2)}
     line = [r[m] if m in r else "" for m in result_metrics]
