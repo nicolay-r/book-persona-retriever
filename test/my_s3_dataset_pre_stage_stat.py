3331d9d41314b9faece3ae8e813cba8835f31cab
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-08-16 14:38:21 +0100

    #34 switched to correct path.

--- test/my_s3_dataset_pre_stage_stat.py
+++ test/my_s3_dataset_pre_stage_stat.py
@@ -3,8 +3,7 @@ from collections import Counter
 from utils_my import MyAPI
 
 fold_index = None
-filepath = MyAPI.dataset_fold_filepath.format(fold_index=fold_index) \
-    if fold_index is not None else MyAPI.dataset_filepath
+filepath = MyAPI.dialogs_filepath
 
 it = MyAPI.iter_dialog_question_response_pairs(dialogs_filapath=filepath, dialogue_filter_func=None)
 
