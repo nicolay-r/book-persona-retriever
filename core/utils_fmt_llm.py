96bb4d3db58f3f735dbce968600debc9c49b9276
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-08-24 16:32:41 +0100

    Added etalon response collection

--- core/utils_fmt_llm.py
+++ core/utils_fmt_llm.py
@@ -5,15 +5,18 @@ def format_episode(request, response, candidates, resp_persona_traits, resp_pers
 
     common = "Consider you're a character from literature novel book. "
 
+    request = request.replace("\t", "")
+
     if resp_persona_traits[0] is None:
         return common + \
                "What would be the response of this character " \
                "for the following utterance: \"{utterance}\". " \
-               "Provide only utterance.".format(utterance=request)
+               "Provide only utterance.".format(utterance=request) \
+               + "\t" + response
     else:
         return common + \
                "with the following humal-level attributes: {traits}. " \
                "What would be the response of this character " \
                "for the following utterance: \"{utterance}\""\
-               "Provide only utterance.".format(traits=",".join(resp_persona_traits),
-                                                utterance=request)
+               "Provide only utterance.".format(traits=",".join(resp_persona_traits), utterance=request) \
+               + "\t" + response
