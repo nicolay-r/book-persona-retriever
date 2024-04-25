7a1e38db95a388d0c06e574e7852675e0866c248
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-04-25 21:37:56 +0100

    #48 removed non utilized anymore gutenberg_dialog scripts.

--- update_gutenberg_dialog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip3 uninstall gutenberg_dialog
-pip3 install git+https://github.com/nicolay-r/gutenberg-dialog
--- uptate_gutenberg_dialog_source.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp -r venv/lib/python3.9/site-packages/gutenberg_dialog/* service/gutenberg-dialog/gutenberg_dialog/
